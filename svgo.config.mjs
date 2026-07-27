7433ce8312e84ad7575c6c3fa008af199adba9ac
julesr0y <95249515+julesr0y@users.noreply.github.com>
2026-07-27 15:30:35 +0200

    chore: updated dependencies & added desc tag

--- svgo.config.mjs
+++ svgo.config.mjs
@@ -12,6 +12,7 @@ export default {
           convertStyleToAttrs: false, // avoid converting styles to attributes
           cleanupNumericValues: false, // avoid converting numeric values to shorter forms (e.g., 0.5 to .5)
           removeXMLNS: false, // avoid removing the xmlns attribute (important for SVGs to be valid)
+          removeDesc: false, // avoid removing the desc attribute/element
         },
       },
     },
